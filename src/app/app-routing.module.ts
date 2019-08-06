134e5e7c1adf8def18b4d8fcdfdda8d54cafee28
Benjamin <benjamin.menzilovic@pragmatio.com>
2019-08-06 10:36:23 +0200

    Added some inputs on signup form that we will adjust in the next lecture.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {SignupComponent} from './auth/signup/signup.component';
 import {LoginComponent} from './auth/login/login.component';
 import {TrainingComponent} from './training/training.component';
+
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
