bc3bd1f45bd8465c376a3cef0c7969e52e938141
Benjamin <benjamin.menzilovic@pragmatio.com>
2019-08-06 11:30:16 +0200

    Singup form with hints and errors.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,21 @@
 import {NgModule} from '@angular/core';
 import {MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    FormsModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    FormsModule
   ]
 })
 export class MaterialModule {}
