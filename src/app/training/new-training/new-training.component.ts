af4bc1481cd0792148208af7e9f36db1161b192e
Benjamin <benjamin.menzilovic@pragmatio.com>
2019-08-07 14:03:17 +0200

    Getting the generated id from table of exercises using snapShotChanges and pipe to map my data into a acceptable form.

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {TrainingService} from '../training.service';
-import {Exercise} from '../exercise.model';
 import {NgForm} from '@angular/forms';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
+import {Exercise} from '../exercise.model';
 
 @Component({
   selector: 'app-new-training',
@@ -11,12 +12,24 @@ import {Observable} from 'rxjs';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  availableExercises: Observable<any>;
+  availableExercises: Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService, private db: AngularFirestore) {}
 
   ngOnInit() {
-    this.availableExercises = this.db.collection('availableExercises').valueChanges();
+    this.availableExercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            name: doc.payload.doc.data()['name'],
+            duration: doc.payload.doc.data()['duration'],
+            calories: doc.payload.doc.data()['calories']
+          };
+        });
+      }));
   }
 
   onStartTraining(form: NgForm) {
